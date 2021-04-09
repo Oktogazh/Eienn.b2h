fcc59dcf0879bfb05ea21ea96db5efd3ff5c43f6
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-04-09 18:20:00 +0200

    error handling

--- server/server.js
+++ server/server.js
@@ -27,6 +27,22 @@ app.use(express.static(__dirname + '/views/'));
 // Defining route middleware
 app.use('/api', require('./routes/api'));
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 // Listening to port
 app.listen(port);
 console.log(`Listening On http://localhost:${port}/api`);
