ce55de8c10583c58bb111512806ad94d20db8d9b
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-05-08 11:34:12 +0200

    api/ezel

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const auth = require('../middlewares/auth.js')
+const auth = require('../middlewares/auth.js');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
@@ -10,7 +11,10 @@ router.get('/', (req, res) => {
   res.json('hello api');
 });
 
-router.post('/ezel', (req, res) => {
+router.post('/ezel',
+  body('email').isEmail().normalizeEmail().trim(),
+  (req, res) => {
+  console.log(req.body.email)
   const email = req.body.email
   User.findOne({'email': email}, (err, user) => {
     res.json({'email': email, 'ezel': !!user})
