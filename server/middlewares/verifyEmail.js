7293b9638366545f56e08d2130a42a97674fb3fd
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-05-09 12:19:51 +0200

    email verification : send email

--- server/middlewares/verifyEmail.js
+++ server/middlewares/verifyEmail.js
@@ -1,33 +1,57 @@
 const User = require('../models/User')
 const EmailCode = require('../models/EmailCode')
 const crypto = require('crypto')
-
+const nodemailer = require('nodemailer')
+const async = require('async')
 
 function randomCode() {
   return crypto.randomBytes(36).toString('hex').slice(2, 8).toUpperCase();
 }
 
 function sendCode(req, res, next) {
-  const code = randomCode()
-  const user = req.user
-  new EmailCode({
-    _userId: user._id,
-    code: code,
-  })
-  // Saving the code in the emailCode collection
-  .save(function (err, code) {
-    if (err) { return res.status(500).send({ msg: err.message }); }
-    res.status(200).json({ code }); // TODO: Send the email
-  })
-/**
- * //Sending the email
-const transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD } });
-            var mailOptions = { from: 'no-reply@yourwebapplication.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
-            transporter.sendMail(mailOptions, function (err) {
-                if (err) { return res.status(500).send({ msg: err.message }); }
-                res.status(200).send('A verification email has been sent to ' + user.email + '.');
-            });
- */
+  async.waterfall([
+    function(done) { // Generate the code
+      const code = randomCode()
+      return done(null, code);
+    },
+    function(code, done) {
+      // Create the code document
+      const user = req.user
+      new EmailCode({
+        _userId: user._id,
+        code: code
+      })
+      // Saving the code in the emailCode collection
+      .save(function (err, code) {
+        if (err) { return res.status(500).send({ msg: err.message }); }
+        console.log(code.code);
+        done(err, user.email, code.code)
+      })
+    },
+    function(email, code, done) {
+      const mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: email,
+        subject: 'Code de vérification',
+        text: 'Voici votre code de validation :\n' +
+        code +
+        '\nVous pouvez rentrer ce code dans la fenêtre qui s\'est ouverte lors de la création de ce mail, ou bien vous pouvez cliquer sur le lien suivant et complèter l\'opération :\n'+
+        process.env.APP_URI + 'gwiriekaat'
+      }
+      // Sends the email
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.EMAIL_ADDRESS, // TODO: use EMAIL_CODE_ADDRESS tomorrow onwards
+          pass: process.env.EMAIL_PASSWORD
+        }
+      })
+      .sendMail(mailOptions, function (err) {
+        if (err) { return res.status(500).send({ msg: err.message }); }
+        res.status(200).send('A verification email has been sent to ' + email + '.');
+      })
+    }
+  ]);
 }
 
 function verify(req, res, next) {
