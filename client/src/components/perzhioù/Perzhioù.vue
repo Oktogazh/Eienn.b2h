fd371acae9bc53a8be005d9ffd014c85d3a7ad0d
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-07-10 12:38:33 +0200

    Munudoù.vue ouzhpennet

--- client/src/components/perzhioù/Perzhioù.vue
+++ client/src/components/perzhioù/Perzhioù.vue
@@ -1,29 +1,34 @@
 <template >
-  <div v-if="!(kontDigor || koumanantDigor)">
+  <div v-if="!(kontDigor || koumanantDigor || munudoùDigor)">
     <h1>Paramètres</h1>
     <h4 @click="digeriñKoumanant" :class="{ 'past-due': $store.state.user.past_due }">Abonnement</h4>
     <h4 @click="kontDigor = true">Mon Compte</h4>
+    <h4 @click="munudoùDigor = true">Détails</h4>
     <h4 @click="digevreañ">Déconnexion</h4>
   </div>
-  <Kont v-if="kontDigor" @klozañ="kontDigor = false"/>
   <Koumanant v-if="koumanantDigor" @klozañ="koumanantDigor = false"/>
+  <Kont v-if="kontDigor" @klozañ="kontDigor = false"/>
+  <Munudoù v-if="munudoùDigor" @klozañ="munudoùDigor = false"/>
 </template>
 
 <script>
-import Kont from './kont/Kont.vue'
 import Koumanant from './koumanant/Koumanant.vue'
+import Munudoù from './munudoù/Munudoù.vue'
+import Kont from './kont/Kont.vue'
 import Swal from 'sweetalert2'
 
 export default {
   name: 'Perzhioù',
   components: {
     Kont,
-    Koumanant
+    Koumanant,
+    Munudoù
   },
   data() {
     return {
       kontDigor: false,
-      koumanantDigor: false
+      koumanantDigor: false,
+      munudoùDigor: false
     }
   },
   methods: {
---
+++ client/src/components/perzhioù/munudoù/Munudoù.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>Mon compte</h1>
+  <h2 @click="$emit('klozañ')">←</h2>
+
+  <h4 @click="daveDigor = !daveDigor">Contact</h4>
+    <h5 v-show="daveDigor">emgav@eienn.bzh</h5>
+  <h4 @click="hentennDigor = !hentennDigor">Methode</h4>
+    <p v-show="hentennDigor">
+      Si la méthode vous plait,
+      sachez qu'elle s'inspire d'une méthode similaire que vous pouvez retrouver à
+      <a href="https://www.saysomethingin.com/">cette adresse</a>.
+    </p>
+  <h4 @click="kudennDigor = !kudennDigor">Problème ?</h4>
+    <p v-show="kudennDigor">
+      Si, pour une raison défiant les lois de la physique,
+      vous préféreriez la version précédente du site, vous pouvez toujours y accéder via
+      <a href="https://v1.eienn.bzh/">ce lien</a>.
+    </p>
+    <p v-show="kudennDigor">
+      Si vous trouvez un bug, merci de le signaler sur
+      <a href="https://github.com/Oktogazh/Eienn.b2h/issues">GitHub</a>.
+    </p>
+</template>
+
+<script>
+//import Swal from 'sweetalert2'
+
+export default {
+  name: 'Munudoù',
+  data() {
+    return {
+      daveDigor: false,
+      hentennDigor: false,
+      kudennDigor: false
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  display: block;
+  float: left;
+  margin-bottom: 4vmax;
+}
+
+h2 {
+  display: block;
+  float: right;
+}
+
+h2:hover,
+h4:hover {
+  cursor: pointer;
+}
+
+h4 {
+  clear: both;
+  text-align: center;
+}
+h5 {
+  clear: both;
+  text-align: center;
+}
+p {
+  clear: both;
+  text-align: center;
+}
+
+</style>
