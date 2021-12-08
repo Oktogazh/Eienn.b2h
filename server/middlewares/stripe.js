8425aa7333e3cb94952de81d7b2692447a8654e5
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-12-08 16:28:10 +0100

    mdlware/stripe: created updateSubscriptions

--- server/middlewares/stripe.js
+++ server/middlewares/stripe.js
@@ -2,6 +2,24 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const User = require('../models/User');
 const { MongoClient } = require('mongodb');
 
+function updateSubscriptions(usersSubs, newSub, productId) {
+  const subscriptionDataToStore = {
+    id: newSub.id,
+    current_period_start: newSub.current_period_start,
+    current_period_end: newSub.current_period_end,
+    productId,
+    status: 'incomplete',
+  };
+  const withoutOldSubAttempts = usersSubs.filter(function(sub, index, arr) {
+      // for security reasons, doesn't delete a sub with the same id if active...
+      return ((sub.productId !== productId) || (sub.status === 'active'));
+  });
+
+  withoutOldSubAttempts.unshift(subscriptionDataToStore);
+
+  return withoutOldSubAttempts;
+};
+
 async function createSubscription(req, res, next) {
   const { customerId } = req.body;
   const { priceId } = req.body;
@@ -10,7 +28,7 @@ async function createSubscription(req, res, next) {
     // Create the subscription. Note we're expanding the Subscription's
     // latest invoice and that invoice's payment_intent
     // so we can pass it to the front end to confirm the payment
-    const subscription = await stripe.subscriptions.create({
+    const newSub = await stripe.subscriptions.create({
       customer: customerId,
       items: [{
         price: priceId,
@@ -19,20 +37,16 @@ async function createSubscription(req, res, next) {
       expand: ['latest_invoice.payment_intent'],
     });
     const user = await User.findOne({ customerId });
-    const { subscriptions } = user;
-    newSubscription = {
-      id: subscription.id,
-      current_period_end: subscription.current_period_end,
-      current_period_start: subscription.current_period_start,
-    }
-    subscriptions.unshift(newSubscription);
+    const usersSubs = user.subscriptions;
+    const productId = newSub.plan.product;
+    const newUsersSubscriptions = await updateSubscriptions(usersSubs, newSub, productId);
 
-    user.subscriptions = subscriptions;
+    user.subscriptions = newUsersSubscriptions;
     user.save();
 
     res.send({
-      subscriptionId: subscription.id,
-      clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+      subscriptionId: newSub.id,
+      clientSecret: newSub.latest_invoice.payment_intent.client_secret,
     });
   } catch (error) {
     return res.status(400).send({ error: { message: error.message } });
